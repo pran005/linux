d28177f0094382e986d62629ea5c576967cc67f9
Mina Almasry <almasrymina@google.com>
2023-11-21 15:26:33 -0800

    [only-internal-review] gve: skb_mark_for_recycle & remove gve recycling
    Upgrade the POC page_pool support with skb_mark_for_recycle()
    & completely disabling GVE page recycling logic.
    
    Verified with devmem TCP to reach 95% line rate.
    
    Signed-off-by: Mina Almasry <almasrymina@google.com>
    
    Change-Id: I8bae3ad73fa3eff0c6fb45d9e7e9c4b76a893202

--- drivers/net/ethernet/google/gve/gve_rx_dqo.c
+++ drivers/net/ethernet/google/gve/gve_rx_dqo.c
@@ -16,16 +16,10 @@
 #include <net/ipv6.h>
 #include <net/tcp.h>
 
-static int gve_buf_ref_cnt(struct gve_rx_buf_state_dqo *bs)
-{
-	return page_count(bs->page_info.page) - bs->page_info.pagecnt_bias;
-}
-
 static void gve_free_page_dqo(struct gve_priv *priv,
 			      struct gve_rx_buf_state_dqo *bs,
 			      bool free_page)
 {
-	page_ref_sub(bs->page_info.page, bs->page_info.pagecnt_bias - 1);
 	if (free_page)
 		gve_free_page(&priv->pdev->dev, bs->page_info.netmem, bs->addr,
 			      DMA_FROM_DEVICE);
@@ -69,98 +63,6 @@ static void gve_free_buf_state(struct gve_rx_ring *rx,
 	rx->dqo.free_buf_states = buffer_id;
 }
 
-static struct gve_rx_buf_state_dqo *
-gve_dequeue_buf_state(struct gve_rx_ring *rx, struct gve_index_list *list)
-{
-	struct gve_rx_buf_state_dqo *buf_state;
-	s16 buffer_id;
-
-	buffer_id = list->head;
-	if (unlikely(buffer_id == -1))
-		return NULL;
-
-	buf_state = &rx->dqo.buf_states[buffer_id];
-
-	/* Remove buf_state from list */
-	list->head = buf_state->next;
-	if (buf_state->next == -1)
-		list->tail = -1;
-
-	/* Point buf_state to itself to mark it as allocated */
-	buf_state->next = buffer_id;
-
-	return buf_state;
-}
-
-static void gve_enqueue_buf_state(struct gve_rx_ring *rx,
-				  struct gve_index_list *list,
-				  struct gve_rx_buf_state_dqo *buf_state)
-{
-	s16 buffer_id = buf_state - rx->dqo.buf_states;
-
-	buf_state->next = -1;
-
-	if (list->head == -1) {
-		list->head = buffer_id;
-		list->tail = buffer_id;
-	} else {
-		int tail = list->tail;
-
-		rx->dqo.buf_states[tail].next = buffer_id;
-		list->tail = buffer_id;
-	}
-}
-
-static struct gve_rx_buf_state_dqo *
-gve_get_recycled_buf_state(struct gve_rx_ring *rx)
-{
-	struct gve_rx_buf_state_dqo *buf_state;
-	int i;
-
-	/* Recycled buf states are immediately usable. */
-	buf_state = gve_dequeue_buf_state(rx, &rx->dqo.recycled_buf_states);
-	if (likely(buf_state))
-		return buf_state;
-
-	if (unlikely(rx->dqo.used_buf_states.head == -1))
-		return NULL;
-
-	/* Used buf states are only usable when ref count reaches 0, which means
-	 * no SKBs refer to them.
-	 *
-	 * Search a limited number before giving up.
-	 */
-	for (i = 0; i < 5; i++) {
-		buf_state = gve_dequeue_buf_state(rx, &rx->dqo.used_buf_states);
-		if (gve_buf_ref_cnt(buf_state) == 0) {
-			rx->dqo.used_buf_states_cnt--;
-			return buf_state;
-		}
-
-		gve_enqueue_buf_state(rx, &rx->dqo.used_buf_states, buf_state);
-	}
-
-	/* For QPL, we cannot allocate any new buffers and must
-	 * wait for the existing ones to be available.
-	 */
-	if (rx->dqo.qpl)
-		return NULL;
-
-	/* If there are no free buf states discard an entry from
-	 * `used_buf_states` so it can be used.
-	 */
-	if (unlikely(rx->dqo.free_buf_states == -1)) {
-		buf_state = gve_dequeue_buf_state(rx, &rx->dqo.used_buf_states);
-		if (gve_buf_ref_cnt(buf_state) == 0)
-			return buf_state;
-
-		gve_free_page_dqo(rx->gve, buf_state, true);
-		gve_free_buf_state(rx, buf_state);
-	}
-
-	return NULL;
-}
-
 static int gve_alloc_page_dqo(struct gve_rx_ring *rx,
 			      struct gve_rx_buf_state_dqo *buf_state)
 {
@@ -195,9 +97,7 @@ static int gve_alloc_page_dqo(struct gve_rx_ring *rx,
 		page_address(buf_state->page_info.page);
 	buf_state->last_single_ref_offset = 0;
 
-	/* The page already has 1 ref. */
-	page_ref_add(buf_state->page_info.page, INT_MAX - 1);
-	buf_state->page_info.pagecnt_bias = INT_MAX;
+	buf_state->page_info.pagecnt_bias = 1;
 
 	return 0;
 }
@@ -250,6 +150,7 @@ static void gve_rx_free_ring_dqo(struct gve_priv *priv, struct gve_rx_ring *rx,
 		if (bs->page_info.netmem && bs->next == i)
 			gve_free_page_dqo(priv, bs, !rx->dqo.qpl);
 	}
+
 	if (rx->dqo.qpl) {
 		gve_unassign_qpl(cfg->qpl_cfg, rx->dqo.qpl->id);
 		rx->dqo.qpl = NULL;
@@ -477,19 +378,16 @@ void gve_rx_post_buffers_dqo(struct gve_rx_ring *rx)
 		struct gve_rx_desc_dqo *desc = &bufq->desc_ring[bufq->tail];
 		struct gve_rx_buf_state_dqo *buf_state;
 
-		buf_state = gve_get_recycled_buf_state(rx);
-		if (unlikely(!buf_state)) {
-			buf_state = gve_alloc_buf_state(rx);
-			if (unlikely(!buf_state))
-				break;
+		buf_state = gve_alloc_buf_state(rx);
+		if (unlikely(!buf_state))
+			break;
 
-			if (unlikely(gve_alloc_page_dqo(rx, buf_state))) {
-				u64_stats_update_begin(&rx->statss);
-				rx->rx_buf_alloc_fail++;
-				u64_stats_update_end(&rx->statss);
-				gve_free_buf_state(rx, buf_state);
-				break;
-			}
+		if (unlikely(gve_alloc_page_dqo(rx, buf_state))) {
+			u64_stats_update_begin(&rx->statss);
+			rx->rx_buf_alloc_fail++;
+			u64_stats_update_end(&rx->statss);
+			gve_free_buf_state(rx, buf_state);
+			break;
 		}
 
 		desc->buf_id = cpu_to_le16(buf_state - rx->dqo.buf_states);
@@ -509,48 +407,6 @@ void gve_rx_post_buffers_dqo(struct gve_rx_ring *rx)
 	}
 
 	rx->fill_cnt += num_posted;
-}
-
-static void gve_try_recycle_buf(struct gve_priv *priv, struct gve_rx_ring *rx,
-				struct gve_rx_buf_state_dqo *buf_state)
-{
-	const u16 data_buffer_size = priv->data_buffer_size_dqo;
-	int pagecount;
-
-	/* Can't reuse if we only fit one buffer per page */
-	if (data_buffer_size * 2 > PAGE_SIZE)
-		goto mark_used;
-
-	pagecount = gve_buf_ref_cnt(buf_state);
-
-	/* Record the offset when we have a single remaining reference.
-	 *
-	 * When this happens, we know all of the other offsets of the page are
-	 * usable.
-	 */
-	if (pagecount == 1) {
-		buf_state->last_single_ref_offset =
-			buf_state->page_info.page_offset;
-	}
-
-	/* Use the next buffer sized chunk in the page. */
-	buf_state->page_info.page_offset += data_buffer_size;
-	buf_state->page_info.page_offset &= (PAGE_SIZE - 1);
-
-	/* If we wrap around to the same offset without ever dropping to 1
-	 * reference, then we don't know if this offset was ever freed.
-	 */
-	if (buf_state->page_info.page_offset ==
-	    buf_state->last_single_ref_offset) {
-		goto mark_used;
-	}
-
-	gve_enqueue_buf_state(rx, &rx->dqo.recycled_buf_states, buf_state);
-	return;
-
-mark_used:
-	gve_enqueue_buf_state(rx, &rx->dqo.used_buf_states, buf_state);
-	rx->dqo.used_buf_states_cnt++;
 }
 
 static void gve_rx_skb_csum(struct sk_buff *skb,
@@ -643,8 +499,7 @@ static int gve_rx_copy_ondemand(struct gve_rx_ring *rx,
 	u64_stats_update_begin(&rx->statss);
 	rx->rx_frag_alloc_cnt++;
 	u64_stats_update_end(&rx->statss);
-	/* Return unused buffer. */
-	gve_enqueue_buf_state(rx, &rx->dqo.recycled_buf_states, buf_state);
+	gve_free_buf_state(rx, buf_state);
 	return 0;
 }
 
@@ -664,6 +519,8 @@ static int gve_rx_append_frags(struct napi_struct *napi,
 		skb = napi_alloc_skb(napi, 0);
 		if (!skb)
 			return -1;
+
+		skb_mark_for_recycle(skb);
 
 		if (rx->ctx.skb_tail == rx->ctx.skb_head)
 			skb_shinfo(rx->ctx.skb_head)->frag_list = skb;
@@ -688,10 +545,7 @@ static int gve_rx_append_frags(struct napi_struct *napi,
 			buf_len, priv->data_buffer_size_dqo);
 	gve_dec_pagecnt_bias(&buf_state->page_info);
 
-	/* Advances buffer page-offset if page is partially used.
-	 * Marks buffer as used if page is full.
-	 */
-	gve_try_recycle_buf(priv, rx, buf_state);
+	gve_free_buf_state(rx, buf_state);
 	return 0;
 }
 
@@ -725,8 +579,8 @@ static int gve_rx_dqo(struct napi_struct *napi, struct gve_rx_ring *rx,
 	}
 
 	if (unlikely(compl_desc->rx_error)) {
-		gve_enqueue_buf_state(rx, &rx->dqo.recycled_buf_states,
-				      buf_state);
+		gve_free_buf_state(rx, buf_state);
+		gve_free_page_dqo(priv, buf_state, true);
 		return -EINVAL;
 	}
 
@@ -753,6 +607,9 @@ static int gve_rx_dqo(struct napi_struct *napi, struct gve_rx_ring *rx,
 		} else {
 			unsplit = 1;
 		}
+
+		skb_mark_for_recycle(rx->ctx.skb_head);
+
 		u64_stats_update_begin(&rx->statss);
 		rx->rx_hsplit_pkt++;
 		rx->rx_hsplit_unsplit_pkt += unsplit;
@@ -781,13 +638,15 @@ static int gve_rx_dqo(struct napi_struct *napi, struct gve_rx_ring *rx,
 			goto error;
 		rx->ctx.skb_tail = rx->ctx.skb_head;
 
+		skb_mark_for_recycle(rx->ctx.skb_head);
+
 		u64_stats_update_begin(&rx->statss);
 		rx->rx_copied_pkt++;
 		rx->rx_copybreak_pkt++;
 		u64_stats_update_end(&rx->statss);
 
-		gve_enqueue_buf_state(rx, &rx->dqo.recycled_buf_states,
-				      buf_state);
+		gve_free_buf_state(rx, buf_state);
+		gve_free_page_dqo(priv, buf_state, true);
 		return 0;
 	}
 
@@ -795,6 +654,8 @@ static int gve_rx_dqo(struct napi_struct *napi, struct gve_rx_ring *rx,
 	if (unlikely(!rx->ctx.skb_head))
 		goto error;
 	rx->ctx.skb_tail = rx->ctx.skb_head;
+
+	skb_mark_for_recycle(rx->ctx.skb_head);
 
 	if (gve_rx_should_trigger_copy_ondemand(rx)) {
 		if (gve_rx_copy_ondemand(rx, buf_state, buf_len) < 0)
@@ -807,11 +668,12 @@ static int gve_rx_dqo(struct napi_struct *napi, struct gve_rx_ring *rx,
 			priv->data_buffer_size_dqo);
 	gve_dec_pagecnt_bias(&buf_state->page_info);
 
-	gve_try_recycle_buf(priv, rx, buf_state);
+	gve_free_buf_state(rx, buf_state);
 	return 0;
 
 error:
-	gve_enqueue_buf_state(rx, &rx->dqo.recycled_buf_states, buf_state);
+	gve_free_buf_state(rx, buf_state);
+	gve_free_page_dqo(priv, buf_state, true);
 	return -ENOMEM;
 }
 
